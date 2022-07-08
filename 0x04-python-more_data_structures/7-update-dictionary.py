c9cced1092626a0666fec0b36506b5d54b57daa5
Heritage <alaoheritage700@gmail.com>
2022-07-08 12:52:20 +0100

    task 7

--- 0x04-python-more_data_structures/7-update-dictionary.py
+++ 0x04-python-more_data_structures/7-update-dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """"
+    updates or add key/value into a dictionary and returns a new copy
+    """
     a_dictionary[key] = value
     return (a_dictionary)
