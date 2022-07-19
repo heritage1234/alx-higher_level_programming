20ec84700094e0cbb6f671fcb99ae25b290e663b
heritage1234 <alaoheritage700@gmail.com>
2022-07-19 17:40:25 +0100

    commited

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
 	count = 0
-	while count < x:
+	for i in range(0, x):
 		try:
-			print("{}".format(my_list[count]), end="")
-		except IndexError:
-				break
-		count += 1
-		print("")
+			print("{:d}".format(my_list[i]), end="")
+			count += 1
+		except Exception:
+				pass
+		print()
 		return count
